a0384eb49d9bffd3eb4324529e92805f9fbc876e
Paulo Lopes Estevão <pl1745240@gmail.com>
2021-05-16 20:16:35 +0100

    base for create blockchain

---
+++ blockchain.py
@@ -0,0 +1,59 @@
+import json
+import datetime
+
+
+
+
+class Block:
+
+
+    def __init__(self,**kwargs):
+        pass
+
+
+    def code_hash(self):
+        pass
+
+
+    def block_data(self):
+        pass
+
+
+
+class BlockChain:
+
+
+    def __init__(self):
+        pass
+
+
+    def genesis_block(self):
+        pass
+
+
+    def build_block(self):
+        pass
+
+
+    def get_last_block(self):
+        pass
+
+
+    def add_transaction(self, sender, receiver, amount, time):
+        pass
+
+
+    def hash(self, block):
+        pass
+
+
+    def proof_of_work(self):
+        pass
+
+
+    def is_chain_valid(self, chain):
+        pass
+
+
+def mine_block():
+    pass
